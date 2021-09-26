3ac7898a2b65240060d6135a33f1216325f30a83
vladkamvl <vladkamvl@gmail.ru>
2021-09-26 23:04:40 +0500

    add routes and controllers

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 require('dotenv').config()
 const express = require('express');
 const sequelize = require('./db');
+const models = require('./models/models');
+const cors = require('cors');
+const router = require('./routes');
+const errorHandlerMiddleware = require('./middleware/ErrorHandlerMiddleware');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
+
+//Error handlers
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
     try {
